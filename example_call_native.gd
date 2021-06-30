061b839f7e84bf7c0182d5def95f58f1085f8911
gilzoide <gilzoide@gmail.com>
2021-06-29 21:45:27 -0300

    Update example

--- example_call_native.gd
+++ example_call_native.gd
@@ -4,6 +4,6 @@ func _ready() -> void:
 	var example = GDNative.new()
 	example.library = preload("res://gdnative/example.gdnlib")
 	example.initialize()  # --> "GDNative initialized"
-	print(example.call_native("native", "MESSAGE", []))  # --> "Hello world!"
-	print(example.call_native("native", "square", [5]))  # --> 25
-	print(example.call_native("native", "sum_ints", [[1, 2.5, 3]]))  # --> 6
+	print(example.call_native("standard_varcall", "get_message", []))  # --> "Hello world!"
+	print(example.call_native("standard_varcall", "square", [5]))  # --> 25
+	print(example.call_native("standard_varcall", "sum_ints", [[1, 2.5, 3]]))  # --> 6
